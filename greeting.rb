74197278619cbe9b5dd4d3060ec5bee108975165
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 18:08:59 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,8 @@
+def greeting_a_person(name, language = "computer")
+  puts "Hello #{name}. We heard you are a great #{language} programmer."
+end
+
+
+greeting_a_person("Hope", "Ruby")
+
+greeting_a_person("Rob")
